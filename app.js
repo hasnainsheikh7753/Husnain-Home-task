63d11c87776a703c62cee4841a060cc85932b606
Hasnain sheikh <husnainsheikh7753@gmail.com>
2023-09-08 03:57:29 +0500

    two file added

---
+++ app.js
@@ -0,0 +1,152 @@
+// chapter 1
+
+// Q 1
+
+var firstName = "Hasnain";
+
+var lastName = "Sheikh";
+
+var email = "hasnainsheikh123@gmail.com";
+
+var phoneNumber = "0123456789";
+
+var password = "*********";
+
+alert("First Name : " + firstName);
+
+alert("Last Name : " + lastName);
+
+alert("Email : " + email);
+
+alert("Phone Number : " + phoneNumber);
+
+alert("Password : " + password);
+
+// Q 2
+
+alert("You're learning JavaScript!");
+
+// Q 3
+
+alert("Hello there i am learning web development!");
+
+// Chapter 2 
+
+// Q 1
+
+var myFirstProject;
+
+// Q 2
+
+var copy;
+
+copy = "Metric Book ";
+
+// Q 3
+
+var teamName = "White House Boys ";
+
+alert(teamName);
+
+// Q 4
+
+var bestMan = "Charlie";
+
+bestMan = "Johnson";
+
+
+// Chapter 3
+
+
+// Q 1
+
+var caseQty;
+
+// Q 2
+
+var caseQty = 144;
+
+// Q 3
+
+var num = 9;
+
+// Q 4
+
+var sum;
+
+sum = 2 + 5;
+
+// Q 5
+
+var marchTotal = 100;
+
+var shippingCharge = 10;
+
+var orderTotal = marchTotal + shippingCharge;
+
+// Q 6
+
+var num3 = 40;
+
+num3 = num3 + 30;
+
+
+// Chapter 4
+
+
+// Q1
+
+var productCost = 3.45;
+
+// Q 2
+
+var nameOfBand;
+
+// Q 3
+
+var myNumber1 = 225;
+
+// Q 4
+
+var fullName = "Hasnain Sheikh";
+
+// Q 5 Legal 
+
+var mobileNumber;
+
+var _phoneNumber;
+
+var myMobileNumber;
+
+// Q 5 Illegal 
+
+var 1phonenumber;
+
+var &mobile;
+
+// Chapter 5
+
+// Q 1
+
+document.write = ( " Name : Modulu   ,  Symbol : % ")
+
+// Q 2
+
+var num9 = 20 % 6;
+
+// Q 3
+
+var largeNum = 1000 * 2000;
+
+// Q 4
+
+
+
+// Q 6 
+
+var num4=25;
+var num5=35;
+
+alert(num4 + num5);
+
+
